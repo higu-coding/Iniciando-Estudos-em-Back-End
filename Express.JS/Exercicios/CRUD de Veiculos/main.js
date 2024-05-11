aedf64efffa5d109583a032e42e0c3eec9c58001
Hiago Candido da Silva <contatohchiago@gmail.com>
2024-05-11 10:57:58 -0300

    novo exercicio de CRUD

--- Express.JS/Exercicios/CRUD de Veiculos/main.js
+++ Express.JS/Exercicios/CRUD de Veiculos/main.js
@@ -25,9 +25,22 @@ app.post('/veiculos', (req, res)=> {
 })
 
 // Vai atualizar os dados de um veiculo
-
+app.put('/veiculos/:id', (req, res) =>{
+    console.log("Veiculo atualizado com sucesso")
+    const veiculo = veiculos.find(x => x.id == req.params.id)
+    const idVeiculo = veiculos.indexOf(veiculo)
+    res.status(200).send("Veiculo atualizado com sucesso")
+})
 
 // Vai excluir um veiculo vendido
+app.delete('/veiculos/:id', (req, res) => {
+    console.log("Veiculo excluido com sucesso")
+    const veiculo = veiculos.find(x => x.id == req.params.id)
+    const idVeiculo = veiculos.indexOf(veiculo)
+    veiculos.splice(idVeiculo, 1)
+    res.status(200).send('Veiculo excluido com sucesso')
+})
+
 
 app.listen(3000, () => {
     console.log("Servidor iniciado.")
